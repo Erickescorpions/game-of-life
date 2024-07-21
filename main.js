374cd8b71f3462c4b0d71c94630ab6b1cf5c2360
Erick Vazquez <erickescorpions@comunidad.unam.mx>
2024-07-20 18:50:22 -0600

    separando funcionalidades en clase board y agregando algunos estilos

--- main.js
+++ main.js
@@ -1,76 +1,26 @@
 import './style.css'
-import { Cell } from './src/cell'
-import { CELL_SIZE, GAME_STATE, TOTAL_HORIZONTAL_CELLS, TOTAL_VERTICAL_CELLS, WIDTH, HEIGHT, CELL_STATE } from './src/consts'
+import { GAME_STATE, WIDTH, HEIGHT } from './src/consts'
+import { Board } from './src/board'
 
 document.querySelector('#app').innerHTML = `
   <canvas id="board" width=${WIDTH} height=${HEIGHT}></canvas>
   <button id="start">Start game</button>
 `
 
-const canvas = document.getElementById('board')
-const ctx = canvas.getContext('2d')
 const startButton = document.getElementById('start')
 
 let gameState = GAME_STATE.STOP
 let intervalId = null
 
-ctx.fillStyle = 'rgb(256, 256, 256)'
-
 // creamos la grilla del canvas
-let board = []
-
-for (let i = 0; i < TOTAL_VERTICAL_CELLS; i++) {
-  let row = []
-  for (let j = 0; j < TOTAL_HORIZONTAL_CELLS; j++) {
-    row.push(new Cell(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE, CELL_STATE.DEAD))
-  }
-  board.push(row)
-}
+let board = new Board()
 
 // draw the grid
-board.forEach(row => row.forEach(col => ctx.stroke(col.draw())))
-
-// agragando on click al canvas
-canvas.addEventListener('click', (event) => {
-  let xPos = event.offsetX
-  let yPos = event.offsetY
-
-  board.forEach(row => {
-    row.forEach(cell => {
-      if(cell.x <= xPos && cell.x + CELL_SIZE > xPos && cell.y <= yPos && cell.y + CELL_SIZE > yPos) {
-        cell.setState(CELL_STATE.LIVE)
-        ctx.fill(cell.draw())
-      }
-    })
-  })
-})
-
-const generateLife = () => {
-  let newBoard = board.map((row, rowIndex) => 
-    row.map((cell, cellIndex) => {
-      let newState = cell.checkCurrentState(board, rowIndex, cellIndex)
-      return new Cell(cell.x, cell.y, cell.width, cell.height, newState)
-    })
-  )
-
-  // Actualizar el estado de la celda
-  board.forEach((row, rowIndex) => {
-    row.forEach((cell, cellIndex) => {
-      cell.setState(newBoard[rowIndex][cellIndex].state)
-      if (cell.state === CELL_STATE.LIVE) {
-        ctx.fill(cell.draw())
-      } else {
-        ctx.clearRect(cell.x, cell.y, CELL_SIZE, CELL_SIZE)
-        ctx.stroke(cell.draw())
-      }
-    })
-  })
-
-}
+board.draw()
 
 const startGame = () => {
   return setInterval(() => {
-    window.requestAnimationFrame(generateLife)
+    window.requestAnimationFrame(() => board.generateLife())
   }, 1000 / 2)
 }
 
@@ -82,9 +32,13 @@ startButton.addEventListener('click', () => {
     gameState = GAME_STATE.STOP
     startButton.innerHTML = "Start game"
     stopGame(intervalId)
+
+    startButton.style.setProperty('--color', '#4CAF50');
   } else {
     gameState = GAME_STATE.START
     startButton.innerHTML = "Stop game"
     intervalId = startGame()
+    
+    startButton.style.setProperty('--color', '#757575');
   }
 })
---
+++ src/board.js
@@ -0,0 +1,69 @@
+import { Cell } from './cell'
+import { CELL_SIZE, GAME_STATE, TOTAL_HORIZONTAL_CELLS, TOTAL_VERTICAL_CELLS, WIDTH, HEIGHT, CELL_STATE } from './consts'
+
+export class Board {
+  constructor() {
+    this.canvas = document.getElementById('board')
+    this.ctx = this.canvas.getContext('2d')
+    
+    // creamos la grilla del canvas
+    this.cells = []
+    
+    for (let i = 0; i < TOTAL_VERTICAL_CELLS; i++) {
+      let row = []
+      for (let j = 0; j < TOTAL_HORIZONTAL_CELLS; j++) {
+        row.push(new Cell(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE, CELL_STATE.DEAD))
+      }
+      
+      this.cells.push(row)
+    }
+    
+
+    this.ctx.fillStyle = 'rgb(256, 256, 256)'
+    // registrando eventos
+    this.canvas.addEventListener('click', (event) => this.reviveCell(event))
+  }
+
+  draw() {
+    // draw the grid
+    this.cells.forEach(row => row.forEach(cell => this.ctx.stroke(cell.draw())))
+  }
+
+  reviveCell(event) {
+    let xPos = event.offsetX
+    let yPos = event.offsetY
+
+    this.cells.forEach(row => {
+      row.forEach(cell => {
+        if(cell.x <= xPos && cell.x + CELL_SIZE > xPos && cell.y <= yPos && cell.y + CELL_SIZE > yPos) {
+          cell.setState(CELL_STATE.LIVE)
+          this.ctx.fill(cell.draw())
+        }
+      })
+    })
+  }
+
+  generateLife() {
+    let newBoard = this.cells.map((row, rowIndex) => 
+      row.map((cell, cellIndex) => {
+        // check state of each cell according the rules
+        let newState = cell.checkCurrentState(this.cells, rowIndex, cellIndex)
+        return new Cell(cell.x, cell.y, cell.width, cell.height, newState)
+      })
+    )
+  
+    // Update the state of the cells
+    this.cells.forEach((row, rowIndex) => {
+      row.forEach((cell, cellIndex) => {
+        cell.setState(newBoard[rowIndex][cellIndex].state)
+        if (cell.state === CELL_STATE.LIVE) {
+          this.ctx.fill(cell.draw())
+        } else {
+          this.ctx.clearRect(cell.x, cell.y, CELL_SIZE, CELL_SIZE)
+          this.ctx.stroke(cell.draw())
+        }
+      })
+    })
+  
+  }
+}
